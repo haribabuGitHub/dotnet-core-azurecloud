767c24b5f9440c2a439d6dbffe0449622db38b9f
haribabu234 <hari.ahb13@gmail.com>
2025-07-23 16:07:00 +0530

    Azure : KeyVault

---
+++ azurekeyvault/azurekeyvault/Program.cs
@@ -0,0 +1,11 @@
+﻿using Azurekeyvault.Services;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        KeyVaultSecretService  azurekeyvault = new KeyVaultSecretService();
+        Console.WriteLine("secrete key "+ azurekeyvault.GetSecreteByKey());
+    }
+}
---
+++ azurekeyvault/azurekeyvault/Services/KeyVaultService.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureKeyVault.Services
+{
+    public interface IKeyVaultSecretService
+    {
+        string GetSecreteByKey();
+    }
+
+    public class KeyVaultSecretService : IKeyVaultSecretService
+    {
+        public KeyVaultSecretService()
+        {
+        }
+
+        public string GetSecreteByKey()
+        {
+          /*  string TanentId = Environment.GetEnvironmentVariable("1d973eb9-4932-4edd-877a-dd1fd9b8ffcd") ?? throw new NullReferenceException();
+            string ApplicationIdUrl = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING") ?? throw new NullReferenceException();
+            string ApplicationId = ApplicationIdUrl.Split("=")[^1];
+            string VaultUrl = "https://zuriskeyvault.vault.azure.net/";
+            string SecreteKey = SfConnectionString.Split('/')[^1];
+            var secretClient = new SecretClient(new Uri(VaultUrl), new ClientSecretCredential(TanentId, ApplicationId, SecreteKey));
+            return secretClient.GetSecret("secretName").Value.Value;*/
+
+            SecretClientOptions options = new ()
+            {
+                Retry =
+            {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+            }
+            };
+            var client = new SecretClient(new Uri("https://zuriskeyvault.vault.azure.net/"), new DefaultAzureCredential(), options);
+            KeyVaultSecret secret = client.GetSecret("k20");
+            string secretValue = secret.Value;
+            return secretValue;
+        }
+    }
+}
